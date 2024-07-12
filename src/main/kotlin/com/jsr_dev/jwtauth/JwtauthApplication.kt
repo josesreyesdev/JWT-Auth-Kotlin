b062adf50dd54cb305cbf38ef6bd6d5777c00924
Jose SR <josesanchez09190@gmail.com>
2024-07-11 21:50:34 -0600

    Add project

---
+++ src/main/kotlin/com/jsr_dev/jwtauth/JwtauthApplication.kt
@@ -0,0 +1,11 @@
+package com.jsr_dev.jwtauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JwtauthApplication
+
+fun main(args: Array<String>) {
+	runApplication<JwtauthApplication>(*args)
+}
---
+++ src/test/kotlin/com/jsr_dev/jwtauth/JwtauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jsr_dev.jwtauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtauthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
